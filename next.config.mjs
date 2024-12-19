e1887d3a43ca57f22a59d9d19640f1301862b696
Tran Khang <procango2003@gmail.com>
2024-12-20 03:59:58 +0700

    too much config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["lh3.googleusercontent.com", "raw.githubusercontent.com"],
+    domains: ['lh3.googleusercontent.com', 'raw.githubusercontent.com'],
   },
-};
+  output: 'standalone',
+}
 
-export default nextConfig;
+export default nextConfig
